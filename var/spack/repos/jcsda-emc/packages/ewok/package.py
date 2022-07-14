ac5ee16784d86d2498d9c1b4e4c1f28efe81fa4a
Dom Heinzeller <dom.heinzeller@icloud.com>
2022-07-14 09:26:11 -0600

    Update JCSDA's ewok, r2d2, solo with new releases

--- var/spack/repos/jcsda-emc/packages/ewok/package.py
+++ var/spack/repos/jcsda-emc/packages/ewok/package.py
@@ -17,7 +17,8 @@ class Ewok(PythonPackage):
     maintainers = ['climbfuji', 'ericlingerfelt']
 
     version('develop', branch='develop', no_cache=True)
-    version('0.0.1', commit='69fff0f460fdb639db4fd38574dee8262b8a1f84', preferred=True)
+    version('0.0.1', commit='69fff0f460fdb639db4fd38574dee8262b8a1f84')
+    version('0.1.0', commit='2f88d2ea99bc88a16154520f69f8d70d4aedafae', preferred=True)
 
     depends_on('python@3.7:',         type=('build', 'run'))
     depends_on('py-pyyaml',           type=('build', 'run'))
