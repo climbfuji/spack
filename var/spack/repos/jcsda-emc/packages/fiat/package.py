fc0c51e3699177d844ee1535f899a25850be0195
Dom Heinzeller <dom.heinzeller@icloud.com>
2022-08-18 10:55:07 -0600

    Fix rpath on macOS in var/spack/repos/jcsda-emc/packages/fiat/package.py

--- var/spack/repos/jcsda-emc/packages/fiat/package.py
+++ var/spack/repos/jcsda-emc/packages/fiat/package.py
@@ -46,3 +46,9 @@ class Fiat(CMakePackage):
         ]
 
         return args
+
+    @run_after("install")
+    def darwin_fix(self):
+        # The shared library is not installed correctly on Darwin; fix this
+        if self.spec.satisfies("platform=darwin"):
+            fix_darwin_install_name(self.prefix.lib)
